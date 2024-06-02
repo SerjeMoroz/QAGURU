e3f070b17bcbe238583885b38eb6005c88f8ee54
Sergei Moroz <moroz.sergei94@gmail.com>
2024-06-02 21:11:46 +0300

    added TextBoxTests

--- src/test/java/TextBoxTests.java
+++ src/test/java/TextBoxTests.java
@@ -2,30 +2,54 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestClassOrder;
+import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.*;
 
 
 public class TextBoxTests extends JUnitExapmles{
 
 
+//    @Test
+//   void fillFormTest() {
+//
+//        open("/text-box");
+//
+//        $("#userName").setValue("Sergei Moroz");
+//        $("#userEmail").setValue("sergei.moroz94test@gmail.com");
+//        $("#currentAddress").setValue("pushkina-kolotushkina");
+//        $("#permanentAddress").setValue("pushkina-kolotushkina2");
+//        $("#submit").click();
+//
+//        $("#output").shouldHave(text("Sergei Moroz"),
+//                text("sergei.moroz94test@gmail.com"),
+//                text("pushkina-kolotushkina"),
+//                text("pushkina-kolotushkina2"));
+//    }
+
     @Test
-   void fillFormTest() {
+    void practiceFormTest() {
+        open("/automation-practice-form");
 
-        open("/text-box");
+        $("#firstName").setValue("Sergei");
+        $("#lastName").setValue("Moroz");
+        $("label[for='gender-radio-1']").click();
+        $("#userNumber").setValue("296506882");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").$(byText("January")).click();
+        $(".react-datepicker__year-select").$(byText("1994")).click();
+        $(".react-datepicker__month-container").$(byText("29")).click();
+        $("label[for='hobbies-checkbox-3']").click();
+//        $("#uploadPicture").uploadFromClasspath("images/Screenshot_1.png");
+        $("#currentAddress").setValue("Ne skajy");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("Uttar Pradesh")).click();
+        $("#city").click();
 
-        $("#userName").setValue("Sergei Moroz");
-        $("#userEmail").setValue("sergei.moroz94test@gmail.com");
-        $("#currentAddress").setValue("pushkina-kolotushkina");
-        $("#permanentAddress").setValue("pushkina-kolotushkina2");
-        $("#submit").click();
-
-        $("#output").shouldHave(text("Sergei Moroz"),
-                text("sergei.moroz94test@gmail.com"),
-                text("pushkina-kolotushkina"),
-                text("pushkina-kolotushkina2"));
     }
+
 
 }
