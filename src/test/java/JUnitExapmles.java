3cf158c52474953967d713241590a5924c54fa9c
Sergei Moroz <moroz.sergei94@gmail.com>
2024-06-02 15:25:54 +0300

    push

---
+++ src/test/java/JUnitExapmles.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+
+
+public class JUnitExapmles {
+
+    @Test
+    void firstTest() {
+        System.out.println("Here is firstTest");
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("Here is secondTest");
+    }
+}
