8f86ccab1161918776f02806090fb57430763657
Sergei Moroz <moroz.sergei94@gmail.com>
2024-06-02 15:36:41 +0300

    push

--- src/test/java/JUnitExapmles.java
+++ src/test/java/JUnitExapmles.java
@@ -1,16 +1,37 @@
-import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.*;
 
 
 public class JUnitExapmles {
 
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Here is beforeAll()");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("Here is beforeEach()");
+    }
+
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("Here is afterAll");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("Here is afterEach");
+    }
+
     @Test
     void firstTest() {
-        System.out.println("Here is firstTest");
+        System.out.println("    Here is firstTest()");
     }
 
     @Test
     void secondTest() {
-        System.out.println("Here is secondTest");
+        System.out.println("    Here is secondTest()");
     }
 }
