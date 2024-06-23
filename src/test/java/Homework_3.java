d4aadd52a943da02c04edef9fb157ee27a9ca1e2
Sergei Moroz <moroz.sergei94@gmail.com>
2024-06-23 21:24:40 +0300

    Homework_3

---
+++ src/test/java/Homework_3.java
@@ -0,0 +1,21 @@
+import com.codeborne.selenide.selector.ByText;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.*;
+
+public class Homework_3 extends JUnitExamples {
+    @Test
+    public void hoverOnGithubPage() {
+        open("https://github.com");
+        $$((".HeaderMenu-link")).findBy(text("Solutions")).hover();
+        $$((".HeaderMenu-dropdown-link")).findBy(text("Enterprise")).click();
+        $(byText("The enterprise-ready platform that developers know and love.")).shouldBe(visible);
+//        $$(".HeaderMenu-link").findBy(text("Solutions")).hover();
+//        $$(".HeaderMenu-dropdown-link").findBy(text("Enterprise")).click();
+//        $(byText("Build like the best")).shouldBe(visible);
+    }
+}
