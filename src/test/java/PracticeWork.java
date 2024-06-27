8394b91a9d9921e9bc546419c53fe1bcbc34d7d1
serjemoroz <moroz.sergei94@gmail.com>
2024-06-27 11:42:13 +0300

    добавлен метод для проверки колонки  и значения сгенерированного

--- src/test/java/PracticeWork.java
+++ src/test/java/PracticeWork.java
@@ -1,9 +1,13 @@
+import static com.codeborne.selenide.CollectionCondition.texts;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
+
 import Hooks.JUnitExamples;
+import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
@@ -17,10 +21,7 @@ import java.util.Random;
 public class PracticeWork extends JUnitExamples {
 
 
-    public static void enterRandomFirstName(SelenideElement field) {
-
-        Faker faker = new Faker();
-        String randomFirstName = faker.name().fullName();
+    public static void enterRandomFirstName(SelenideElement field, String randomFirstName) {
 
         field.shouldBe(enabled, Duration.ofSeconds(10));
         field.sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.BACK_SPACE);
@@ -29,7 +30,7 @@ public class PracticeWork extends JUnitExamples {
         assert field.getValue().equals(randomFirstName) : "Поле ввода не содержит текст: " + randomFirstName;
     }
 
-    public static void enterRandomLastName(SelenideElement field) {
+    public static void enterRandomLastName(SelenideElement field, String LastName) {
 
         Faker faker = new Faker();
         String randomLastName = faker.name().lastName();
@@ -42,7 +43,7 @@ public class PracticeWork extends JUnitExamples {
 
     }
 
-    public static void enterRandomEmail(SelenideElement field) {
+    public static void enterRandomEmail(SelenideElement field, String randomEmailAddress) {
         Faker faker = new Faker();
         String randomEmail = faker.internet().emailAddress();
 
@@ -79,15 +80,18 @@ public class PracticeWork extends JUnitExamples {
     public void testElementsTextBox() {
 
         Configuration.timeout = 1000;
+        Faker faker = new Faker();
+        String randomFirstName = faker.name().fullName();
+        String randomEmail = faker.internet().emailAddress();
 
         open("/elements");
 
         $(By.id("item-0")).click();
         SelenideElement nameField = $(By.id("userName"));
-        enterRandomFirstName(nameField);
+        enterRandomFirstName(nameField, randomFirstName);
 
         SelenideElement emailField = $(By.id("userEmail"));
-        enterRandomEmail(emailField);
+        enterRandomEmail(emailField, randomEmail);
 
         SelenideElement currentAddressField = $(By.id("currentAddress"));
         enterRandomAddress(currentAddressField);
@@ -136,37 +140,61 @@ public class PracticeWork extends JUnitExamples {
         $(byText("You have selected")).shouldHave(text(expectedText));
     }
 
+    public void findLocatorInTable(String column, String value) {
+        assert  $(byXpath("//div[text()='" + column + "']/ancestor::div[@class='rt-table']//div[@class='rt-tr-group']//div[text()='" + value + "']")).is(visible, Duration.ofSeconds(10)) : "Значение " + value + "в " +column + "не найдено";
+    }
+
+
     @Test
     public void testElementsWebtables() {
+        Faker faker = new Faker();
+        String randomFirstName = faker.name().firstName();
+        String randomLastName = faker.name().lastName();
+        String randomEmail = faker.internet().emailAddress();
 
         open("/webtables");
 
+        // Добавляем неявное ожидание после загрузки страницы
         $(byId("addNewRecordButton")).click();
         $("#registration-form-modal").shouldHave(text("Registration Form"));
 
         SelenideElement firstNameField = $("#firstName");
-        enterRandomFirstName(firstNameField);
+        enterRandomFirstName(firstNameField, randomFirstName);
 
         SelenideElement lastNameField = $("#lastName");
-        enterRandomLastName(lastNameField);
+        enterRandomLastName(lastNameField, randomLastName);
 
         SelenideElement emailField = $("#userEmail");
-        enterRandomEmail(emailField);
+        enterRandomEmail(emailField, randomEmail);
 
         $("#age").setValue("54");
         $("#salary").setValue("545");
         $("#department").setValue("5454");
         $("#submit").click();
 
-        $(By.xpath("//h1[normalize-space()='Web Tables']")).shouldHave(text("Web Tables"));
+        // Добавляем неявное ожидание после отправки формы
+        $(By.xpath("//h1[normalize-space()='Web Tables']")).shouldBe(visible);
 
-        $(byXpath("//div[contains(text(),'First Name')]")).shouldHave(text(firstNameField.getValue()));
-        $(byXpath("//div[contains(text(),'Last Name')]")).shouldHave(text(lastNameField.getValue()));
-        $(byXpath("//div[contains(text(),'Email')]")).shouldHave(text(emailField.getValue()));
+
+
+        findLocatorInTable("First Name", randomFirstName);
+////        $(byXpath("//div[@role='grid']")).shouldHave(text(firstNameField.getValue()));
+////        $(byXpath("//div[contains(text(),'Last Name')]")).shouldHave(text(lastNameField.getValue()));
+////        $(byXpath("//div[contains(text(),'Email')]")).shouldHave(text(emailField.getValue()));
 ////        $(byXpath("//div[contains(text(),'Age')]")).shouldHave(value("54"));
 ////        $(byXpath("//div[contains(text(),'Salary')]")).shouldHave(value("545"));
-//        $(byXpath("//div[normalize-space()='']")).shouldHave(value("5454"));
+////        $(byXpath("//div[normalize-space()='']")).shouldHave(value("5454"));
 
+
+
+
+//    @Test
+//    public void testElementsClicks() {
+//
+//        open("/buttons");
+//
+//        $("#doubleClickBtn").doubleClick();
+//        $("#doubleClickMessage").shouldHave(text("You have done a double click"));
+//    }
     }
-
-}
+}//div[@class='rt-tr-group']//div[text()='Cierra']
